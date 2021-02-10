21077d1a76200c24e0fb7cdffd4728be598ad30f
Manish Poduval <manishpoduval@hotmail.com>
2021-02-10 18:37:03 +0530

    added a msg varaible in app

--- app.js
+++ app.js
@@ -55,4 +55,7 @@ app.use("/", authRoutes);
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
+// New feature
+let msg = 'Hello Ironhackers'
+
 module.exports = app;
